c3814036d8fcc8a7db9d2bf767dea4739179f463
Joe Engel <engel.joe@me.com>
2014-04-23 15:31:35 -0500

    Update removeServer.py

--- cloudServers/removeServer.py
+++ cloudServers/removeServer.py
@@ -7,6 +7,7 @@ import sys
 import time
 import pyrax
 
+pyrax.set_setting("identity_type", "rackspace")
 credsFile = os.path.expanduser("~/.rackspace_cloud_credentials")
 pyrax.set_credential_file(credsFile)
 
