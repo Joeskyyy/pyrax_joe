5c6ab0c16aca10f6985e3b90b69aef55fde08f4c
Joe Engel <engel.joe@me.com>
2014-04-23 15:33:02 -0500

    Update deleteContainer.py

--- cloudFiles/deleteContainer.py
+++ cloudFiles/deleteContainer.py
@@ -7,6 +7,7 @@ import os
 import pyrax
 
 # Credentials
+pyrax.set_setting("identity_type", "rackspace")
 creds_file = os.path.expanduser("~/.rackspace_cloud_credentials")
 pyrax.set_credential_file(creds_file)
 cf = pyrax.cloudfiles
